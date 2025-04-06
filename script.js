c49ee85e5a49b9cd88972985c926cc43a200a72e
medvid.t <medvidd.t98@gmail.com>
2025-04-07 01:27:33 +0300

    Fixed caching for repo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register("/sw.js")
+            .register("/PI/sw.js")
             .then((reg) => console.log("Service Worker registered with scope:", reg.scope))
             .catch((err) => console.error("Service Worker registration failed:", err));
     });
